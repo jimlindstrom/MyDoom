851f8ac6b4dcceb653c5333794655b9f0d8982a7
Jim Lindstrom <jim.lindstrom@gmail.com>
2014-12-07 12:36:14 -0500

    Refactored a few items from node.h into their own headers

---
+++ hdr/partition_line.h
@@ -0,0 +1,15 @@
+#ifndef __PARTITION_LINE_H
+#define __PARTITION_LINE_H
+
+#include <stdint.h>
+
+class partition_line
+{
+public:
+  int16_t x;
+  int16_t y;
+  int16_t dx;
+  int16_t dy;
+};
+
+#endif
